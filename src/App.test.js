be0cf5d88ab3f10ff6f76a8ca66e8f87a502f98a
Gaurav <gaurav1223344@gmail.com>
2023-06-01 00:39:26 +0530

    added test while app is in docker container

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
