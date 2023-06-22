552ac95da92cd67fe234e8c96c1cf96c88d2824c
Gaurav <gaurav1223344@gmail.com>
2023-06-22 16:53:32 +0530

    Applied Nginx

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I AM GAURAV SHARMA</p> 
+        <p>I AM GAURAV SHARMAfd</p> 
         <a
           className="App-link"
           href="https://reactjs.org"
