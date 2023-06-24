2b0f86191f005b8d1d88c6ed6584f788ee403183
Gaurav <gaurav1223344@gmail.com>
2023-06-24 18:25:52 +0530

    updates test

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/visit my website/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/visit my website/i);
   expect(linkElement).toBeInTheDocument();
 });
 
